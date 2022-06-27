5d2ddfa2b1dc2f5a9d4993d16b62b45782ee41a5
MehvishSiddiqui <mehvishsiddiqui115@gmail.com>
2022-06-27 16:55:04 +0530

    First commit

---
+++ src/javaBranch/JavaBranch.java
@@ -0,0 +1,15 @@
+package javaBranch;
+
+public class JavaBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		String a ="this is Sam and Tina";
+		
+		System.out.println(a);
+		
+}
+
+}
